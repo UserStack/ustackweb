74b5b2f808d7c09b9b26030bc6e954e2fb881f6c
Jens Bissinger <mail@jens-bissinger.de>
2014-09-24 11:04:58 +0200

    Use structs for users.

--- src/ustackweb/controllers/users.go
+++ src/ustackweb/controllers/users.go
@@ -1,5 +1,10 @@
 package controllers
 
+type User struct {
+  Id int
+  Name string
+}
+
 type UsersController struct {
   BaseController
 }
@@ -13,5 +18,12 @@ func (this *UsersController) Prepare() {
 
 func (this *UsersController) Get() {
   this.TplNames = "users/index.tpl.html"
-  this.Data["users"] = []string{"foo","admin"}
+  this.Data["users"] = []User{User{Id:1,Name:"foo"},
+                              User{Id:2,Name:"admin"},
+                              User{Id:3,Name:"abc"},
+                              User{Id:4,Name:"def"},
+                              User{Id:5,Name:"hij"},
+                              User{Id:6,Name:"glk"},
+                              User{Id:7,Name:"uvw"},
+                              User{Id:8,Name:"xyz"}}
 }
