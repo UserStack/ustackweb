8e8be9f172d9bc323a014e4f03cad7d5cc2623d5
Jens Bissinger <mail@jens-bissinger.de>
2014-09-24 11:53:14 +0200

    Use paginator from wetalk.

--- src/ustackweb/controllers/users.go
+++ src/ustackweb/controllers/users.go
@@ -1,5 +1,9 @@
 package controllers
 
+import (
+  "ustackweb/utils"
+)
+
 type User struct {
   Id int
   Name string
@@ -18,12 +22,23 @@ func (this *UsersController) Prepare() {
 
 func (this *UsersController) Get() {
   this.TplNames = "users/index.tpl.html"
-  this.Data["users"] = []User{User{Id:1,Name:"foo"},
-                              User{Id:2,Name:"admin"},
-                              User{Id:3,Name:"abc"},
-                              User{Id:4,Name:"def"},
-                              User{Id:5,Name:"hij"},
-                              User{Id:6,Name:"glk"},
-                              User{Id:7,Name:"uvw"},
-                              User{Id:8,Name:"xyz"}}
+  users := []User{User{Id:1,Name:"foo"},
+                  User{Id:2,Name:"admin"},
+                  User{Id:3,Name:"abc"},
+                  User{Id:4,Name:"def"},
+                  User{Id:5,Name:"hij"},
+                  User{Id:6,Name:"glk"},
+                  User{Id:7,Name:"uvw"},
+                  User{Id:8,Name:"xyz"}}
+
+  paginator := utils.NewPaginator(this.Ctx.Request, 3, len(users))
+  this.Data["paginator"] = paginator
+  this.Data["users"] = users[paginator.Offset():min(paginator.Offset()+paginator.PerPageNums, len(users))]
+}
+
+func min(x, y int) int {
+  if x > y {
+    return y
+  }
+  return x
 }
