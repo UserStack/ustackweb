b95aeb37bf8a1563e0f71cf807fa6abe0462bc56
Jens Bissinger <mail@jens-bissinger.de>
2014-09-25 11:40:13 +0200

    Extract models package.

--- src/ustackweb/controllers/users.go
+++ src/ustackweb/controllers/users.go
@@ -1,8 +1,8 @@
 package controllers
 
 import (
-	"github.com/UserStack/ustackd/backends"
 	"github.com/astaxie/beego"
+	"ustackweb/models"
 	"ustackweb/utils"
 )
 
@@ -17,29 +17,9 @@ func (this *UsersController) Prepare() {
 	this.Layout = "layouts/default.tpl.html"
 }
 
-func (this *UsersController) allUsers() []backends.User {
-	return []backends.User{backends.User{Uid: 1, Email: "foo"},
-		backends.User{Uid: 2, Email: "admin"},
-		backends.User{Uid: 3, Email: "abc"},
-		backends.User{Uid: 4, Email: "def"},
-		backends.User{Uid: 5, Email: "hij"},
-		backends.User{Uid: 6, Email: "glk"},
-		backends.User{Uid: 7, Email: "uvw"},
-		backends.User{Uid: 8, Email: "xyz"}}
-}
-
-func (this *UsersController) findUser(uid int) backends.User {
-	for _, user := range this.allUsers() {
-		if user.Uid == uid {
-			return user
-		}
-	}
-	return backends.User{}
-}
-
 func (this *UsersController) Index() {
 	this.TplNames = "users/index.tpl.html"
-	users := this.allUsers()
+	users := models.AllUsers()
 	paginator := utils.NewPaginator(this.Ctx.Request, 3, len(users))
 	this.Data["paginator"] = paginator
 	this.Data["users"] = users[paginator.Offset():utils.Min(paginator.Offset()+paginator.PerPageNums, len(users))]
@@ -48,12 +28,12 @@ func (this *UsersController) Index() {
 func (this *UsersController) Edit() {
 	this.TplNames = "users/edit.tpl.html"
 	id, _ := this.GetInt(":id")
-	this.Data["user"] = this.findUser(int(id))
+	this.Data["user"] = models.FindUser(int(id))
 }
 
 func (this *UsersController) Update() {
 	id, _ := this.GetInt(":id")
-	user := this.findUser(int(id))
+	user := models.FindUser(int(id))
 	flash := beego.NewFlash()
 	flash.Notice("Updated user " + user.Email)
 	flash.Store(&this.Controller)
@@ -62,7 +42,7 @@ func (this *UsersController) Update() {
 
 func (this *UsersController) Destroy() {
 	id, _ := this.GetInt(":id")
-	user := this.findUser(int(id))
+	user := models.FindUser(int(id))
 	flash := beego.NewFlash()
 	flash.Notice("Deleted user " + user.Email)
 	flash.Store(&this.Controller)
