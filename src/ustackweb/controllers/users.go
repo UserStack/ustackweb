d08082aabaa78d3dc06ce884c7c2602b62b515ca
Jens Bissinger <mail@jens-bissinger.de>
2014-09-25 10:03:03 +0200

    Cleanup users data handling.

--- src/ustackweb/controllers/users.go
+++ src/ustackweb/controllers/users.go
@@ -17,29 +17,42 @@ func (this *UsersController) Prepare() {
   this.Layout = "layouts/default.tpl.html"
 }
 
+func (this *UsersController) allUsers() []backends.User {
+  return []backends.User{backends.User{Uid:1,Email:"foo"},
+                         backends.User{Uid:2,Email:"admin"},
+                         backends.User{Uid:3,Email:"abc"},
+                         backends.User{Uid:4,Email:"def"},
+                         backends.User{Uid:5,Email:"hij"},
+                         backends.User{Uid:6,Email:"glk"},
+                         backends.User{Uid:7,Email:"uvw"},
+                         backends.User{Uid:8,Email:"xyz"}}
+}
+
+func (this *UsersController) findUser(uid int) backends.User {
+  for _, user := range this.allUsers() {
+      if user.Uid == uid {
+          return user
+      }
+  }
+  return backends.User{}
+}
+
 func (this *UsersController) Index() {
   this.TplNames = "users/index.tpl.html"
-  users := []backends.User{backends.User{Uid:1,Email:"foo"},
-                           backends.User{Uid:2,Email:"admin"},
-                           backends.User{Uid:3,Email:"abc"},
-                           backends.User{Uid:4,Email:"def"},
-                           backends.User{Uid:5,Email:"hij"},
-                           backends.User{Uid:6,Email:"glk"},
-                           backends.User{Uid:7,Email:"uvw"},
-                           backends.User{Uid:8,Email:"xyz"}}
+  users := this.allUsers()
   paginator := utils.NewPaginator(this.Ctx.Request, 3, len(users))
   this.Data["paginator"] = paginator
   this.Data["users"] = users[paginator.Offset():utils.Min(paginator.Offset()+paginator.PerPageNums, len(users))]
 }
 
 func (this *UsersController) Edit() {
-  // this.GetString(":id")
   this.TplNames = "users/edit.tpl.html"
-  this.Data["user"] = backends.User{Uid:1,Email:"foo"}
+  id, _ := this.GetInt(":id")
+  this.Data["user"] = this.findUser(int(id))
 }
 
 func (this *UsersController) Update() {
-  // this.GetString(":id")
+  // this.GetInt(":id")
   flash := beego.NewFlash()
   flash.Notice("Updated")
   flash.Store(&this.Controller)
